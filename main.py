3c7160e9ed64f1eaeb82738d1a25331a14d1a6a1
Thomas Evan Spackman <61983349+spackperson@users.noreply.github.com>
2025-05-21 21:54:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from microbit import *
 import random
 
-while True:
-    gameStarted = False
+#variables
+score_A = 0
+score_B = 0
+gameOn = True
+
+#start image
+display.show(Image.PACMAN)
+
+while gameOn:
+    roundStarted = False
     if pin0.is_touched():
+        #countdown and display start image
         display.show(3)
         sleep(1000)
         display.show(2)
@@ -12,20 +21,37 @@ while True:
         sleep(1000)
         display.show(Image.YES)
         sleep(1000)
+
+        #de
         sleep(random.randint(1000, 5000))
-        gameStarted = True
+        roundStarted = True
         display.show(Image.HEART)
         start = running_time()
-        while gameStarted:
+        while roundStarted:
             if pin1.is_touched():
                 display.show('A')
                 sleep(1000)
                 display.scroll(running_time() - start)
-                gameStarted = False
+                score_A += 1
+                roundStarted = False
             elif pin2.is_touched():
                 display.show('B')
                 sleep(1000)
                 display.scroll(running_time()- start)
-                gameStarted = False
+                score_B += 1
+                roundStarted = False
         sleep(3000)
         display.clear()
+    elif button_a.is_pressed() and button_b.is_pressed():
+            gameOn = False
+
+#print end game Stats
+#maybe add best reation time here, need to build lists etc.
+if score_A > score_B:
+    A_wins = "A wins with" + str(score_A) + "points!"
+    display.scroll(A_wins, 70)
+elif score_B > score_A:
+    B_wins = "B wins with" + str(score_B) + "points!"
+    display.scroll(B_wins, 70)
+else:
+    display.scroll("It's a tie!")
